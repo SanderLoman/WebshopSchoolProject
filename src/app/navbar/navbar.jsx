d6d95ca5d7bd476de9f51292efeb55c8295964e5
SanderLoman <sanderfeitsma13@gmail.com>
2023-06-21 13:45:39 +0200

    adding primsa database

--- src/app/navbar/navbar.jsx
+++ src/app/navbar/navbar.jsx
@@ -1,5 +1,23 @@
-import "./navbar.css"
+"use client";
+
+import React from "react";
+import "./navbar.css";
 
 export default function Navbar() {
-  return <nav className="bg-pink-500 h-16">dfsdfsdf</nav>;
+  return (
+    <nav className="z-10 navbar absolute w-full p-4 text-lg sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">
+      <div className="wrapper flex w-full">
+        <div className="w-1/2">
+          <ul className="flex space-x-2 md:space-x-4 lg:space-x-8 xl:space-x-16">
+            <li>HOME</li>
+            <li>TEST</li>
+            <li>TEST</li>
+          </ul>
+        </div>
+        <div className="flex justify-end w-1/2">
+          <a href="">LOGIN</a>
+        </div>
+      </div>
+    </nav>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import Navbar from "./navbar/navbar";
+import Hero from "./hero/hero";
 
 export default function Home() {
-  return (<Navbar></Navbar>);
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hero></Hero>
+    </>
+  );
 }
