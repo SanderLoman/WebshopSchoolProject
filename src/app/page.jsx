7506f3f5625e128741ca8decf7bf5f32f350d117
SanderLoman <sanderfeitsma13@gmail.com>
2023-06-21 14:58:54 +0200

    changed few things

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./navbar/navbar";
 import Hero from "./hero/hero";
+import Section from "./section/section";
 
 export default function Home() {
   return (
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+      <Section></Section>
     </>
   );
 }
---
+++ src/app/section/section.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './section.css';
+
+export default function Section() {
+    return (
+        <div className='h-screen flex justify-center items-center bg-red-500'>
+            
+        </div>
+    )
+}
