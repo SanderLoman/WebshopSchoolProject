04da11839f3546dce42aa6978b200c6dc0a07c0f
SanderLoman <sanderfeitsma13@gmail.com>
2023-09-19 13:54:20 +0200

    setting up views for admin page

--- webshop/src/components/admin/Admin.jsx
+++ webshop/src/components/admin/Admin.jsx
@@ -7,17 +7,22 @@ const Admin = () => {
     const [isOpen, setIsOpen] = useState(false)
     const [dropdownWidth, setDropdownWidth] = useState(null)
     const [windowWidth, setWindowWidth] = useState(window.innerWidth)
+    const [currentView, setCurrentView] = useState("orders")
     const { user, logout } = useAuth()
     const navigate = useNavigate()
     const parentRef = useRef(null)
 
     useEffect(() => {
         console.log("User state has changed:", user)
-    }, [user])
+
+        if (user && user.role !== "admin") {
+            navigate("/login")
+        }
+    }, [user, navigate])
 
     useEffect(() => {
         if (isOpen && parentRef.current) {
-            setDropdownWidth(parentRef.current.offsetWidth + 10)
+            setDropdownWidth(parentRef.current.offsetWidth + 19)
         }
     }, [isOpen])
 
@@ -41,6 +46,10 @@ const Admin = () => {
 
     const navigateToHome = () => {
         navigate("/")
+    }
+
+    const switchView = (view) => {
+        setCurrentView(view)
     }
 
     const menuItems = (
@@ -91,7 +100,7 @@ const Admin = () => {
                                     <ul className="cursor-pointer">
                                         {!user && (
                                             <li
-                                                className="border-b p-2 hover:bg-slate-100 active:bg-slate-200 rounded-bl-xl"
+                                                className="border-b p-2 hover:bg-slate-100 active:bg-slate-200 rounded-bl-xl text-start"
                                                 onClick={navigateToLogin}
                                             >
                                                 <div>Login</div>
@@ -99,7 +108,7 @@ const Admin = () => {
                                         )}
                                         {user && (
                                             <li
-                                                className="p-2 hover:bg-slate-100 active:bg-slate-200 rounded-bl-xl"
+                                                className="p-2 hover:bg-slate-100 active:bg-slate-200 rounded-bl-xl text-start"
                                                 onClick={() => {
                                                     logout()
                                                     navigate("/")
@@ -118,26 +127,68 @@ const Admin = () => {
             <div className="flex justify-center h-full w-screen font-bold">
                 <div className="container">
                     {/* Buttons */}
-                    <div className="flex justify-between mt-14 p-4">
-                        {/* Buttons on the top-left */}
+                    <div className="flex justify-between mt-14 p-2 md:p-0 md:py-2">
                         <div className="flex w-1/2 space-x-2">
-                            <button className="border-2 border-black px-2 bg-white">
+                            <button
+                                onClick={() => switchView("orders")}
+                                className={`border-2 border-black px-2 h-10 transition-all duration-75 ease-in-out ${
+                                    currentView === "orders"
+                                        ? "bg-blue-600 text-white"
+                                        : "bg-white hover:bg-blue-200"
+                                }`}
+                            >
                                 Orders
                             </button>
-                            <button className="border-2 border-black px-2 bg-white">
+                            <button
+                                onClick={() => switchView("products")}
+                                className={`border-2 border-black px-2 h-10 transition-all duration-75 ease-in-out ${
+                                    currentView === "products"
+                                        ? "bg-blue-600 text-white"
+                                        : "bg-white hover:bg-blue-200"
+                                }`}
+                            >
                                 Products
                             </button>
                         </div>
                         {/* Buttons on the top-right */}
-                        <div className="flex w-1/2 justify-between md:justify-end space-x-2">
-                            <button className="border-2 border-black px-2 bg-white hover:bg-red-600 hover:text-white transition-all duration-150 ease-in-out">
-                                Reset Products
-                            </button>
-                            <button className="w-10 h-10 border-2 border-black bg-white">
-                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAABFUlEQVR4nO2aSwrCMBRFzzbqVF2TvzVoXYaTbsztKDqMCBFEtAW1MTXnwB0VShOa9+67BEREREREJDEjYB5VURgr4AiEqAOwpBCmwPlu8TedgAkFsH2y+Js2FEDTsgHXZ39P4wbgHxA8AlgDgkUQu0CwDaIPCBohdIJBK8ywZoEpUAO7N7RvscL7N99ZpxylVy/m+V/rnCJUGT0kObnp2He8tshgkV26Zoy9Mc9ggV2a9bkBVQwwQ6Y6pEiYlzHADJnpFI9oEsYxwMylDW7iNw2C5h+M0Ce4AfgH4BHAGoBFELsAtkH0AWiE0AmiFaagWaBuscJrCmDScklqMCPtN0KVx2tyycKMXKhKvigpIiIiIsJvuQBJNvgVdT6bCQAAAABJRU5ErkJggg==" />
-                            </button>
+                        <div className="flex w-1/2 md:justify-end space-x-2">
+                            {currentView === "products" && (
+                                <>
+                                    <button className="h-10 border-2 border-black px-2 bg-white hover:bg-red-600 hover:text-white transition-all duration-75 ease-in-out">
+                                        Reset Products
+                                    </button>
+                                    <button className="w-10 h-10 border-2 border-black bg-white hover:bg-green-500 hover:text-white transition-all duration-75 ease-in-out">
+                                        <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAABFUlEQVR4nO2aSwrCMBRFzzbqVF2TvzVoXYaTbsztKDqMCBFEtAW1MTXnwB0VShOa9+67BEREREREJDEjYB5VURgr4AiEqAOwpBCmwPlu8TedgAkFsH2y+Js2FEDTsgHXZ39P4wbgHxA8AlgDgkUQu0CwDaIPCBohdIJBK8ywZoEpUAO7N7RvscL7N99ZpxylVy/m+V/rnCJUGT0kObnp2He8tshgkV26Zoy9Mc9ggV2a9bkBVQwwQ6Y6pEiYlzHADJnpFI9oEsYxwMylDW7iNw2C5h+M0Ce4AfgH4BHAGoBFELsAtkH0AWiE0AmiFaagWaBuscJrCmDScklqMCPtN0KVx2tyycKMXKhKvigpIiIiIsJvuQBJNvgVdT6bCQAAAABJRU5ErkJggg==" />
+                                    </button>
+                                </>
+                            )}
                         </div>
                     </div>
+                    {currentView === "orders" && (
+                        <div className="border-2 border-black mx-2 md:mx-0">
+                            <table className="min-w-full bg-white">
+                                <thead>
+                                    <tr>
+                                        <th className="w-1/4 py-2 px-4 border">
+                                            ID
+                                        </th>
+                                        <th className="w-1/4 py-2 px-4 border">
+                                            Name
+                                        </th>
+                                        <th className="w-1/4 py-2 px-4 border">
+                                            Buyer
+                                        </th>
+                                        <th className="w-1/4 py-2 px-4 border">
+                                            Timestamp
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {/* Need to GET orders here and map them*/}
+                                </tbody>
+                            </table>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
