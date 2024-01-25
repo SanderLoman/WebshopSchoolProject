7917c817413c0d54a800674c75197b13cf480869
SanderLoman <sanderfeitsma13@gmail.com>
2024-01-25 19:57:31 +0100

    Update Cart component styling

--- webshop/src/components/cart/Cart.jsx
+++ webshop/src/components/cart/Cart.jsx
@@ -52,7 +52,7 @@ const Cart = ({ showCart, setshowCart }) => {
                     </div>
                     {/* Table View */}
                     {cartItems.length > 0 ? (
-                        <div className="relative overflow-x-auto max-h-[calc(50vh)] overflow-y-auto">
+                        <div className="relative overflow-y-auto max-h-[calc(50vh)]">
                             <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                                 <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                                     <tr>
@@ -74,7 +74,7 @@ const Cart = ({ showCart, setshowCart }) => {
                                     {cartItems.map((item) => (
                                         <tr
                                             key={item.id}
-                                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 "
                                         >
                                             <td className="p-4">
                                                 <img
