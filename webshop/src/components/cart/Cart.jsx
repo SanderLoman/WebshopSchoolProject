cca3bddc81066769910fa3b83dd392129ef880e0
SanderLoman <sanderfeitsma13@gmail.com>
2024-01-26 20:06:00 +0100

    Refactor Cart component to remove clearCart function and add close button icon

--- webshop/src/components/cart/Cart.jsx
+++ webshop/src/components/cart/Cart.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom"
 import "./Cart.css"
 
 const Cart = ({ showCart, setshowCart }) => {
-    const { cartItems, clearCart, removeFromCart } = useContext(CartContext)
+    const { cartItems, removeFromCart } = useContext(CartContext)
     const navigate = useNavigate()
 
     // Temp
@@ -49,7 +49,19 @@ const Cart = ({ showCart, setshowCart }) => {
                             className="text-gray-400 bg-transparent hover:text-gray-900 rounded-lg h-8 ml-auto inline-flex justify-center items-center dark:hover:text-white"
                             onClick={() => setshowCart(false)}
                         >
-                            Close
+                            <svg
+                                aria-hidden="true"
+                                class="w-5 h-5"
+                                fill="currentColor"
+                                viewBox="0 0 20 20"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    fill-rule="evenodd"
+                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"
+                                ></path>
+                            </svg>
                         </button>
                     </div>
                     {/* Table View */}
