72d172adf361b293ea17c14d547a02b7e7ee9adf
SanderLoman <sanderfeitsma13@gmail.com>
2024-01-30 22:45:58 +0100

    added UserContext

--- webshop/src/App.js
+++ webshop/src/App.js
@@ -8,6 +8,7 @@ import Account from "./components/account/Account.jsx"
 import Page404 from "./components/errors/Page404.jsx"
 import AccessDenied from "./components/errors/AccessDenied.jsx"
 import { ThemeProvider } from "./components/providers/ThemeProvider.jsx"
+import UserContext from "./components/providers/UserContext.jsx"
 import CartProvider from "./components/providers/CartProvider.jsx"
 import Checkout from "./components/checkout/Checkout.jsx"
 import "flowbite"
@@ -16,23 +17,28 @@ import "./App.css"
 function App() {
     return (
         <ThemeProvider>
-            <CartProvider>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<HomePage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/register" element={<RegisterPage />} />
-                        <Route
-                            path="/access-denied"
-                            element={<AccessDenied />}
-                        />
-                        <Route path="/admin" element={<AdminPage />} />
-                        <Route path="/account" element={<Account />} />
-                        <Route path="*" element={<Page404 />} />
-                        <Route path="/checkout" element={<Checkout />} />
-                    </Routes>
-                </Router>
-            </CartProvider>
+            <UserContext>
+                <CartProvider>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/login" element={<LoginPage />} />
+                            <Route
+                                path="/register"
+                                element={<RegisterPage />}
+                            />
+                            <Route
+                                path="/access-denied"
+                                element={<AccessDenied />}
+                            />
+                            <Route path="/admin" element={<AdminPage />} />
+                            <Route path="/account" element={<Account />} />
+                            <Route path="*" element={<Page404 />} />
+                            <Route path="/checkout" element={<Checkout />} />
+                        </Routes>
+                    </Router>
+                </CartProvider>
+            </UserContext>
         </ThemeProvider>
     )
 }
